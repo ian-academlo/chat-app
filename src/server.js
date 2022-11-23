82a3f6ee3040a9c1a3bcf4a956d8527726117ac3
ianAcademlo <ian.rosas@academlo.com>
2022-11-22 21:19:05 -0600

    basic config on swagger

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const app = require("./app");
+const swaggerDocs = require("../swagger");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`servidor corriendo en el puerto ${PORT}`);
+  swaggerDocs(app, PORT);
 });
---
+++ swagger.js
@@ -0,0 +1,35 @@
+// importamos las dos depencias que acabamos de instalar
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Clon de un chat en node js",
+      version: "1.0.0",
+      description: "API que sirve para crear una aplicación de mensajera.",
+    },
+  },
+  apis: ["./src/routes/users.routes.js", "./src/models/users.models.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+// función para configruar la documentación
+// dos parametros --> app expres, port donde se ejecuta
+const swaggerDocs = (app, port) => {
+  // manejador para la ruta de nuestra documentación
+  app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  // podemos definir nuestra documentación en formato json
+  app.get("/api/v1/docs.json", (req, res) => {
+    res.setHeader("ContentType", "application/json");
+    res.send(swaggerSpec);
+  });
+  //
+  console.log(
+    `Documentación disponible en http://localhost:${port}/api/v1/docs`
+  );
+};
+
+module.exports = swaggerDocs;
